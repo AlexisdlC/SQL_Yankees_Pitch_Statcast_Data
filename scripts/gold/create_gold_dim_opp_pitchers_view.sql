2cb0f15c039307458e696fc7cdea055ac3fe500b
AlexisdlC <alexis.delacotte@gmail.com>
2026-02-13 17:39:59 -0500

    Create create_gold_dim_opp_pitchers_view.sql

---
+++ scripts/gold/create_gold_dim_opp_pitchers_view.sql
@@ -0,0 +1,22 @@
+/*
+=================================================================================
+Create view to extract create dimension table containing Yankees' opponent's
+pitcher info
+=================================================================================
+*/
+
+IF OBJECT_ID('gold.dim_opp_pitchers', 'V') IS NOT NULL
+	DROP VIEW  gold.dim_opp_pitchers
+GO
+
+CREATE VIEW gold.dim_opp_pitchers AS
+
+SELECT
+	player_id,
+	-- Concatenate and capitalize first and last name of player
+	CONCAT(UPPER(SUBSTRING(name_first,1,1)),SUBSTRING(name_first,2,LEN(name_first)), ' ',
+	UPPER(SUBSTRING(name_last,1,1)),SUBSTRING(name_last,2,LEN(name_last))) AS player_name,
+	--Extract year only for first and last seasons
+	YEAR(first_season) AS first_season,
+	YEAR(last_season) AS last_season
+FROM silver.opp_pitchers
