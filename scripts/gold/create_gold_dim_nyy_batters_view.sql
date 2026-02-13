1af45317de336a1c57cf8779fb11abe891353439
AlexisdlC <alexis.delacotte@gmail.com>
2026-02-13 16:19:04 -0500

    Update create_gold_dim_nyy_batters_view.sql

--- scripts/gold/create_gold_dim_nyy_batters_view.sql
+++ scripts/gold/create_gold_dim_nyy_batters_view.sql
@@ -12,8 +12,10 @@ CREATE VIEW gold.dim_nyy_batters AS
 
 SELECT
 	player_id,
+	-- Concatenate and capitalize first and last name of player
 	CONCAT(UPPER(SUBSTRING(name_first,1,1)),SUBSTRING(name_first,2,LEN(name_first)), ' ',
 	UPPER(SUBSTRING(name_last,1,1)),SUBSTRING(name_last,2,LEN(name_last))) AS player_name,
+	--Extract year only for first and last seasons
 	YEAR(first_season) AS first_season,
 	YEAR(last_season) AS last_season
 FROM silver.batters
